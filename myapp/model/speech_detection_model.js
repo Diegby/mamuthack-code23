0b2ee7b4bf4702e4675da257d0dc0692ad22d8f2
romOlivo <al341918@uji.es>
2019-10-05 16:41:50 +0200

    Test model 1 not working

---
+++ myapp/model/speech_detection_model.js
@@ -0,0 +1,12 @@
+const sk = require('scikit-learn');
+const fs = require('fs');
+const pickle = require('pickle');
+const MODEL_ING_TASK_1 = './../resources/model_task1.pkl';
+
+exports.identifyTwits = function(data, callback){
+    fs.readFile(MODEL_ING_TASK_1, function(err,file){
+        pickle.loads(file, function(pickled) {
+            callback(pickled)
+        })
+    })
+}
