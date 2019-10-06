e74f33a683132913cea746cb375953bb5441f15f
Jorge Lengua <al341875@uji.es>
2019-10-06 07:39:54 +0200

    Delete files done

--- myapp/controller/user.js
+++ myapp/controller/user.js
@@ -1,34 +1,42 @@
 var sdm = require("./../model/speech_detection_model");
-const tmodel = require("./../model/tweets_model")
+const tmodel = require("./../model/tweets_model");
 var spawn = require("child_process").spawn;
-const WORDS_ES = './resources/hurtlex_ES.tsv';
 const fs = require('fs');
 
-const POSITION_VALUE = 0
-const CATEGORY_VALUE = 1
-const STEREOTYPE_VALUE = 2
-const LEMMA_VALUE = 3
+const WORDS_ES = './resources/hurtlex_ES.tsv';
+
+const LASER_LOWER_DATA = "/tmp/laserLower.raw";
+const TEXT_LOWER_DATA = "/tmp/testLower.txt";
+const JSON_DATA = '/tmp/tweets.json';
+const LASER_DATA = "/tmp/laser.raw";
+const TEST_DATA = "/tmp/test.txt";
+
+const STEREOTYPE_VALUE = 2;
+const CATEGORY_VALUE = 1;
+const POSITION_VALUE = 0;
 const LEVEL_VALUE = 4;
+const LEMMA_VALUE = 3;
 
 exports.getTwitsData = function(usuario, callback){
     tmodel.getTwits(usuario, function(tweets){
         console.log(tweets)
         let data = JSON.stringify({data: tweets});
-        fs.writeFile('/tmp/tweets.json', data, 'utf8', function(err){
-            fs.writeFile("/tmp/test.txt", tweets, function(err) {
+        fs.writeFile(JSON_DATA, data, 'utf8', function(err){
+            fs.writeFile(TEST_DATA, tweets, function(err) {
     
-                var sh = spawn('sh',['model/external/laser/tasks/embed/embed.sh', "/tmp/test.txt", "es", "/tmp/laser.raw"]);
+                var sh = spawn('sh',['model/external/laser/tasks/embed/embed.sh', TEST_DATA, "es", LASER_DATA]);
                 sh.on('exit', function(code) {
                     for (let i in tweets) {
                         tweets[i] = tweets[i].toLowerCase();
                     }
         
-                    fs.writeFile("/tmp/testLower.txt", tweets, function(err) {
+                    fs.writeFile(TEXT_LOWER_DATA, tweets, function(err) {
         
-                        var sh = spawn('sh',['model/external/laser/tasks/embed/embed.sh', "/tmp/testLower.txt", "es", "/tmp/laserLower.raw"]);
+                        var sh = spawn('sh',['model/external/laser/tasks/embed/embed.sh', TEXT_LOWER_DATA, "es", LASER_LOWER_DATA]);
                         sh.on('exit', function (code) {
-                            var pyProg = spawn('python3',['model/external/nn.py', "/tmp/tweets.json", "/tmp/laser.raw", "/tmp/laserLower.raw"]); 
+                            var pyProg = spawn('python3',['model/external/nn.py', JSON_DATA, LASER_DATA, LASER_LOWER_DATA]); 
                             pyProg.stdout.on('data', function(data){
+                                delete_all_files()
                                 callback(data.toString())
                             });
                         });
@@ -63,4 +71,11 @@ function get_dictionary(infoTwits, callback){
         }
         callback(infoTwits)
     })
+}
+
+function delete_all_files(){
+    FILES_TO_REMOVE = [LASER_LOWER_DATA, TEXT_LOWER_DATA, JSON_DATA, LASER_DATA, TEST_DATA]
+    for (let i in FILES_TO_REMOVE){
+        fs.unlink(FILES_TO_REMOVE[i])
+    }
 }
